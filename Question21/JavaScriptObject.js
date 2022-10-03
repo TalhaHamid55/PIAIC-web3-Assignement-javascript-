1fc8a0b4836507c08d0a80520775a66d39442956
TalhaHamid55 <talha.hamid55@gmail.com>
2022-10-04 01:56:35 +0500

    ALL Answers

---
+++ Question21/JavaScriptObject.js
@@ -0,0 +1,9 @@
+//Create Object
+let object = new Object();
+object["name"] = "Talha Hamid";
+object["age"] = 24;
+object["education"] = "BS Computer Engineering";
+console.log(object);
+
+
+
---
+++ Question44/Sandwiches.js
@@ -0,0 +1,12 @@
+function sandwich(items){
+    var selected="|";
+    items.map((item)=>{
+        selected+=item+"|";
+    })
+    console.log("Person Selected "+selected+" on his sandwich")
+
+}
+
+sandwich(["Cheese","Onion","Chicken"])
+sandwich(["Cheese","Hard-boiled egg","Sause"])
+sandwich(["Cheese","tikka","masala"]) 
